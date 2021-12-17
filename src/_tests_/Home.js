9ed355fab4129dc2b3fe9c7b4a180cdfb96bb67b
kevin-adams <ka-jr048@outlook.com>
2021-12-17 12:47:28 -0800

    update

--- src/_tests_/Home.js
+++ src/_tests_/Home.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import {useHistory} from 'react-router-dom'
 
 export default function Home() {
-  return (
-    <div className="home">
-      <Link to="/pizza">
-        <img alt="brick oven pizza" src="https://images.unsplash.com/photo-1536622308015-0740925b8221?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"/>
-      </Link>
-    </div>
-  );
+    const history = useHistory()
+
+    const routeToOrderForm = () => {
+        console.log(history)
+        history.push('/Form.js')
+    }
+    
+    return (
+        <div className = 'homeContainer'>
+            <img
+            className = 'home-image'
+            src = '../Assets/Pizza.jpg'
+            alt = 'pizzaImg' />
+            <button
+                onClick = {routeToOrderForm}
+                className = 'orderBtn'>
+                ORDER NOW
+            </button>
+        </div>
+    )
+
 }
